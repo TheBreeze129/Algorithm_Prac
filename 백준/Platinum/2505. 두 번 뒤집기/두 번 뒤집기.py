4cfc202eeb32f62d4a4a6ec92faa8d0a4729da61
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2024-10-07 12:50:32 +0900

    [Platinum V] Title: 두 번 뒤집기, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Platinum/2505. 두 번 뒤집기/두 번 뒤집기.py
@@ -0,0 +1,40 @@
+from itertools import *
+
+def find(seq):
+  start=end=0
+  for i in range(N+1):
+    if i!=seq[i]:
+      if not start:
+        start=i
+      end = i
+  return (start,end) if start else (1,1)
+
+def reverse(seq,start,end):
+  return seq[:start]+[*reversed(seq[start:end+1])]+seq[end+1:]
+
+N = int(input())
+seq = [0]+[*map(int,input().split())]
+
+start,end = find(seq)
+
+interval = []; s = start
+while s<=end:
+  interval.append(s)
+  e = s
+  while e<end:
+    if abs(seq[e+1]-seq[e])==1:
+      e += 1
+    else:
+      break
+  interval.append(e)
+  s = e+1
+  
+seq0 = [i for i in range(N+1)]
+for s,e in combinations(interval,2):
+  s1,e1 = min(s,e),max(s,e)
+  seq1 = reverse(seq,s1,e1)
+  s2,e2 = find(seq1)
+  if reverse(seq1,s2,e2)==seq0:
+    result = [(s1,e1),(s2,e2)]
+for s,e in result:
+  print(s,e)
