3d1c6953c75e2f1a5e5b9c63371777bd0a45b56a
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2024-11-22 04:56:21 +0900

    Title: 바이러스 검사, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241122/바이러스 검사/virus-detector.py
+++ 241122/바이러스 검사/virus-detector.py
@@ -10,7 +10,7 @@ answer = 0
 for num in customers:
     answer += 1
     num -= lead
-    while num > 0:
+    answer += (num // mate)
+    if num % mate != 0:
         answer += 1
-        num -= mate
 print(answer)
