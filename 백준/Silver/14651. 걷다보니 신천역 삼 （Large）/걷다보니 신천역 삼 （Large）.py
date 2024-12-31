98e3b1c4ed6ba14a9bcfe7a40c063d47df100173
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2024-12-31 17:39:01 +0900

    [Silver I] Title: 걷다보니 신천역 삼 (Large), Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/14651. 걷다보니 신천역 삼 （Large）/걷다보니 신천역 삼 （Large）.py
@@ -0,0 +1,8 @@
+import sys 
+N = int(sys.stdin.readline())
+dp = [0 for _ in range(N+1)]
+if N >= 2:
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] * 3) % 1000000009
+print(dp[N])
