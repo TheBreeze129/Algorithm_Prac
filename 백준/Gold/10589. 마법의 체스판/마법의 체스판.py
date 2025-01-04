d9c11963d4141a68ea8ec4979fa3b5691986de62
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2025-01-04 18:50:16 +0900

    [Gold IV] Title: 마법의 체스판, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10589. 마법의 체스판/마법의 체스판.py
+++ 백준/Gold/10589. 마법의 체스판/마법의 체스판.py
@@ -1,5 +1,7 @@
 N,M = map(int,input().split())
-ans = [(1, j, N, j) for j in range(2, M+1, 2)] + [(i, 1, i, M) for i in range(2, N+1, 2)]
-print(len(ans))
-for t in ans:
-    print(*t)
+p=print
+r=range
+a = [(1, j, N, j) for j in r(2, M+1, 2)] + [(i, 1, i, M) for i in r(2, N+1, 2)]
+p(len(a))
+for t in a:
+    p(*t)
