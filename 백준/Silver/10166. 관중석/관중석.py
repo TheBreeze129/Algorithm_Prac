927d2f02ac476e1c2c6e1aa17d0af5913c7dca02
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2025-02-01 11:22:07 +0900

    [Silver I] Title: 관중석, Time: 892 ms, Memory: 65044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,14 +1,8 @@
-from sys import stdin
 from math import gcd
-
-a, b = map(int, stdin.readline().split())
-arr = [[0] * b for _ in range(b)]
-ans = 0
-for i in range(a, b + 1):
-    for j in range(1, i + 1):
-        g = gcd(i, j)
-        x, y = i // g, j // g
-        if not arr[x-1][y-1]:
-            arr[x-1][y-1] = 1
-            ans += 1
-print(ans)
+D1, D2 = map(int, input().split())
+count = [[0 for _ in range(2001)] for _ in range(2001)]
+for i in range(D1, D2+1):
+    for j in range(1, i):
+        k = gcd(i, j)
+        count[j//k][i//k] = 1
+print(sum(sum(count[i]) for i in range(2001))+1)
