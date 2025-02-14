02dd248e1a6e8fbb4e066d264c9a91d8da46be2f
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2025-02-14 18:58:47 +0900

    [Bronze I] Title: Meats On The Grill, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.py
+++ 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.py
@@ -1,6 +1,3 @@
-N = int(input())
-for i in range(N):
-    a, b = map(int, input().split())
-    x = [input()[::-1] for _ in range(a)]
-    for i in x:
-        print(i)
+for _ in range(int(input())):
+    for j in range(int(input().split()[0])):
+        print(input()[::-1])
