579ec2422160c4f301123989d98046264065303b
Hyeongwoo, Lee <52536141+TheBreeze129@users.noreply.github.com>
2025-03-29 18:34:42 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.cc
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.cc
@@ -1,39 +1,39 @@
-#include <bits/stdc++.h>
-#define INF 0x3f3f3f3f
-using namespace std;
-using pii = pair<int,int>;
-int a, b, n, m;
-vector <int> graph[1001];
-vector <int> dist(1001);
-int dijkstra(){
-    priority_queue <pii,vector<pii>,greater<pii>> pq;
-    pq.push({0,a});
-    dist[a] = 0;
-    while(!pq.empty()){
-        int cur = pq.top().second;
-        int cost = pq.top().first;
-        pq.pop();
-        if(cost > dist[cur]) continue;
-        for(auto next : graph[cur]){
-            if(cost + 1 < dist[next]){
-                dist[next] = cost + 1;
-                pq.push({cost+1,next});
-            }
-        }
-    }
-    if(dist[b] == INF) return -1;
-    return dist[b];
-}
-
-int main(){
-    cin >> a >> b;
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++) dist[i] = INF;
-    for(int i = 0; i < m; i++){
-        int u,v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    cout << dijkstra();
+#include <iostream>
+#include <queue>
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	int a, b, c, d, t1, t2;
+	std::cin >> a >> b >> c >> d;
+	std::vector<int> graph[1000];
+	std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+	for (int i = 0; i < d; i++) {
+		std::cin >> t1 >> t2;
+		graph[t1 - 1].push_back(t2 - 1);
+		graph[t2 - 1].push_back(t1 - 1);
+	}
+	bool visited[1000];
+	for (int i = 0; i < 1000; i++) {
+		visited[i] = false;
+	}
+	pq.push({ 0, a - 1 });
+	while (!pq.empty()) {
+		int cur = pq.top().second;
+		int cost = pq.top().first;
+		pq.pop();
+		visited[cur] = true;
+		if (cur == b - 1) {
+			std::cout << cost;
+			return 0;
+		}
+		for (int x : graph[cur]) {
+			if (!visited[x]) {
+			    visited[x] = true;
+				pq.push({ cost + 1, x });
+			}
+		}
+	}
+	std::cout << -1;
+	return 0;
 }
